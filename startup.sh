4723ceeb53f5c315a0dababaa239dfe807380e05
hannah98 <hannah98@users.noreply.github.com>
2016-11-15 10:25:17 -0600

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+appdir=/var/www/html
+index=$appdir/index.php
+apache=/usr/local/bin/apache2-foreground
+
+if [ ! -f $index ]; then
+    echo Downloading Chevereto installer
+    wget --quiet --output-document=$index https://cdn.rawgit.com/Chevereto/php-repo-installer/master/index.php
+    if [ $? -ne 0 ]; then
+        echo Could not downolad installer... failing
+        exit 1
+    fi
+    chown www-data:www-data $index
+fi
+
+echo "Starting Apache2 ($apache)"
+$apache
